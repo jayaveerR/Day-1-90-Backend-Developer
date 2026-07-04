7f02df40cae758041918d5b5708c1aac458481fe
Jayaveer <ramanadhamjayaveer@mictech.edu.in>
2026-07-04 17:29:30 +0530

    Day-1/90

---
+++ Variable.py
@@ -0,0 +1,132 @@
+# variables in python - store data using Names
+
+
+name='jayaveer'
+age=20
+print(name,age)
+
+
+# Module 2: Variable Declaration
+
+name='jayaveer'
+age=23
+salary=50000
+print(name,age,salary)
+
+# Module 3: Variable Naming Rules
+
+salary
+name
+age
+
+
+# Data Types
+
+# integer
+age = 20
+
+#float
+age = 20.5
+
+# Boolean
+is_login =  True
+is_logout = False
+
+#string
+
+name = "jayaveer"
+
+# None 
+
+user = None
+
+# Module-5 Type()
+
+name = 'jayaveer'
+print(type(name))
+
+# Module-6 Multiple Variables
+
+a,b,c=10,20,30
+print(a,b,c)
+
+# Module 7: Same Value
+x=y=z=100
+print(x,y,z)
+
+# Module 8: Variable Reassignment
+
+age = 115
+age = 5
+age = 1
+print(age)
+
+# Module 9: Dynamic Typing
+x = 10
+x = "Python"
+x = True
+x = 56
+print(type(x))
+
+# Module 10: Input()
+
+name = input("Enter Your Name ")
+
+
+# Module 11: Casting
+age = int(input("Enter Age "))
+
+
+# Module 12: print()
+# print(name)
+
+# Module 13: f-string
+name = "Jayaveer i Am From Mangalagiri " 
+print(f"My Name is {name}")
+
+# Module 14: Memory Concept
+x=10
+y=x
+print(x)
+
+# Module 15: id()
+
+x = 0
+print(id(x))
+
+# Module 16: Constants
+PI=3.14
+
+# Commits
+
+"""
+Hii
+Hello World!
+"""
+# Module 18: Keywords
+
+import keyword
+
+print(keyword.kwlist)
+
+# Module 19: Variable Scope (Introduction)
+
+name = "Jay"
+
+# Module 20: Best Practices
+student_name 
+
+
+'''
+🧠 Interview Questions
+Variable ante enti?
+Python lo datatype declare cheyyala?
+Dynamic typing ante enti?
+type() use enti?
+id() use enti?
+input() return type enti?
+int() enduku use chestam?
+None ante enti?
+True and False datatype enti?
+Python naming conventions enti?
+'''
